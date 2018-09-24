ebd3e6bad20b0ac03aff1be0f26c8df9271a746c
nsh578 <nsh5784@gmail.com>
2018-09-24 23:46:43 +0900

    Modified errors in NYTjson file and generated articles in 2000.

--- NYTjson.py
+++ NYTjson.py
@@ -9,50 +9,50 @@ sys.setdefaultencoding('utf8')
 
 import requests
 """
-    About:
-    Python wrapper for the New York Times Archive API
-    https://developer.nytimes.com/article_search_v2.json
-    """
+About:
+Python wrapper for the New York Times Archive API 
+https://developer.nytimes.com/article_search_v2.json
+"""
 
 class APIKeyException(Exception):
-    def __init__(self, message): self.message = message
+    def __init__(self, message): self.message = message 
 
 class InvalidQueryException(Exception):
-    def __init__(self, message): self.message = message
+    def __init__(self, message): self.message = message 
 
 class ArchiveAPI(object):
     def __init__(self, key=None):
         """
-            Initializes the ArchiveAPI class. Raises an exception if no API key is given.
-            :param key: New York Times API Key
-            """
+        Initializes the ArchiveAPI class. Raises an exception if no API key is given.
+        :param key: New York Times API Key
+        """
         self.key = key
-        self.root = 'http://api.nytimes.com/svc/archive/v1/{}/{}.json?api-key={}'
+        self.root = 'http://api.nytimes.com/svc/archive/v1/{}/{}.json?api-key={}' 
         if not self.key:
             nyt_dev_page = 'http://developer.nytimes.com/docs/reference/keys'
             exception_str = 'Warning: API Key required. Please visit {}'
             raise NoAPIKeyException(exception_str.format(nyt_dev_page))
 
-def query(self, year=None, month=None, key=None,):
-    """
+    def query(self, year=None, month=None, key=None,):
+        """
         Calls the archive API and returns the results as a dictionary.
         :param key: Defaults to the API key used to initialize the ArchiveAPI class.
         """
-            if not key: key = self.key
-                if (year < 1882) or not (0 < month < 13):
-                    # currently the Archive API only supports year >= 1882
-                    exception_str = 'Invalid query: See http://developer.nytimes.com/archive_api.json'
-                        raise InvalidQueryException(exception_str)
-                            url = self.root.format(year, month, key)
-                            r = requests.get(url)
-                            return r.json()
+        if not key: key = self.key
+        if (year < 1882) or not (0 < month < 13):
+            # currently the Archive API only supports year >= 1882
+            exception_str = 'Invalid query: See http://developer.nytimes.com/archive_api.json'
+            raise InvalidQueryException(exception_str)
+        url = self.root.format(year, month, key)
+        r = requests.get(url)
+        return r.json()
 
 
 api = ArchiveAPI('e718f6d0a51845ec8532741e7c1aa876')
 
 
 
-years = [i for i in range(2000, 2018)]
+years = [i for i in range(2000, 2001)]
 months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 
 for year in years:
@@ -62,6 +62,6 @@ for year in years:
         with open(file_str, 'w') as fout:
             json.dump(mydict, fout)
         fout.close()
+        
 
-
-
+    
